eef43e2bfd0f51d3458483bc3b63965318be6e00
Burak Ege Kaya <burakegekaya1903@gmail.com>
2025-05-07 23:23:27 +0300

    changing auth.js res.cookie-->httpOnly to secure

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -48,7 +48,7 @@ router.post("/login", async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production", // 💡 buraya dikkat
+      secure: true,
       sameSite: "None",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
